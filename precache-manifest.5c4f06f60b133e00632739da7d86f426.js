b9234366c22611f02b2a1f8bc75af495c559c352
stfreevk <61653818+stfreevk@users.noreply.github.com>
2020-03-01 15:47:31 +0300

    Add files via upload

---
+++ precache-manifest.5c4f06f60b133e00632739da7d86f426.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fabf7f4da97c6f76406df8ee2aa03be",
+    "url": "/konkurs/index.html"
+  },
+  {
+    "revision": "97cb94fbb4a92908037a",
+    "url": "/konkurs/static/css/2.3e9c613c.chunk.css"
+  },
+  {
+    "revision": "e9bbedf35206c327a001",
+    "url": "/konkurs/static/css/main.224f0a7f.chunk.css"
+  },
+  {
+    "revision": "97cb94fbb4a92908037a",
+    "url": "/konkurs/static/js/2.cfb37517.chunk.js"
+  },
+  {
+    "revision": "e9bbedf35206c327a001",
+    "url": "/konkurs/static/js/main.f9afc581.chunk.js"
+  },
+  {
+    "revision": "c0267249ccc120a7c683",
+    "url": "/konkurs/static/js/runtime-main.865cb39f.js"
+  },
+  {
+    "revision": "bd011f2da3d19cb3e07e812fc58a33db",
+    "url": "/konkurs/static/media/bg.bd011f2d.png"
+  },
+  {
+    "revision": "1e0fc11f7867bf32cd538a47a8dab5ca",
+    "url": "/konkurs/static/media/button.1e0fc11f.png"
+  }
+]);
